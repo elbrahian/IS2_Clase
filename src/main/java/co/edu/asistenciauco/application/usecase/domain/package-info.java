558c642665ae180733bfbc7b44796cb81ee0e5ee
elbrahian <brahianrueda123@gmail.com>
2025-04-10 21:08:42 -0500

    Initial commit

---
+++ src/main/java/co/edu/asistenciauco/application/usecase/domain/package-info.java
@@ -0,0 +1,1 @@
+package co.edu.asistenciauco.application.usecase.domain;
---
+++ src/main/java/co/edu/asistenciauco/initializater/AsistenciaucoApplication.java
@@ -0,0 +1,13 @@
+package co.edu.asistenciauco.initializater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsistenciaucoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsistenciaucoApplication.class, args);
+    }
+
+}
